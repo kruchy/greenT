0c8ddf603bce0c41d66ef807c016efe625152be6
krzysiek <kruchy61@gmail.com>
2023-05-08 11:11:22 -0700

    Add some common spring classes

---
+++ src/main/java/com/misiak/transactions/config/SpringSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.misiak.transactions.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic()
+                .disable()
+                .build();
+    }
+}
